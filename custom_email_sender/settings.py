18c78a43f7977963cbc414e5d5d895da67136e21
Rachit <rachitkum123456@gmail.com>
2025-03-18 18:41:48 +0530

    Update settings.py

--- custom_email_sender/settings.py
+++ custom_email_sender/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',  # Add this before django.contrib.staticfiles
     'django.contrib.staticfiles',
     'corsheaders',
 ]
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
